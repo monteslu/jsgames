0fb24cf81cc2877d63d388a753e5e99212b2ac4d
Luis Montes <monteslu@gmail.com>
2024-12-10 14:26:42 -0700

    intial

---
+++ tuxgame/sound.js
@@ -0,0 +1,48 @@
+// import sdl from '@kmamal/sdl'
+
+const { sdl } = globalThis;
+
+const TWO_PI = 2 * Math.PI
+
+const playbackInstance = sdl.audio.openDevice({ type: 'playback' });
+
+
+const {
+	channels,
+	frequency,
+	bytesPerSample,
+	minSampleValue,
+	maxSampleValue,
+	zeroSampleValue,
+} = playbackInstance
+const range = maxSampleValue - minSampleValue
+const amplitude = range / 2
+
+const sineAmplitude = 0.3 * amplitude
+const sineNote = 440
+const sinePeriod = 1 / sineNote
+
+const duration = 0.2
+const numFrames = duration * frequency
+const numSamples = numFrames * channels
+const numBytes = numSamples * bytesPerSample
+const buffer = Buffer.alloc(numBytes)
+
+let offset = 0
+for (let i = 0; i < numFrames; i++) {
+	const time = i / frequency
+	const angle = time / sinePeriod * TWO_PI
+	const sample = zeroSampleValue + Math.sin(angle) * sineAmplitude
+	for (let j = 0; j < channels; j++) {
+		offset = playbackInstance.writeSample(buffer, sample, offset)
+	}
+}
+
+
+
+function play() {
+  playbackInstance.enqueue(buffer);
+  playbackInstance.play()
+}
+
+export default play;
